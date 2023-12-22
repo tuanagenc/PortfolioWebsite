7ec76130263578d21ca5ee30926cf01b62aa1e02
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2023-12-22 15:47:53 +0300

    Breakfoınts Added

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// toogle icon navbar (geçiş simgesi gezinme çubuğu)
+let menuIcon = document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+}
+
+//scroll section (kaydırma bölümü)
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            //active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+    //sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+
