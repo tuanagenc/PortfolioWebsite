5a58facf8d7524b7e592e4c857ea97ac445163ae
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2023-12-27 10:58:58 +0300

    KEYFRAMES ANIMATION added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // toogle icon navbar (geçiş simgesi gezinme çubuğu)
 let menuIcon = document.querySelector('#menu-icon');
-let navbar =document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
@@ -24,8 +24,16 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active section for animation on scroll 
+            sec.classList.add('show-animate');
+
+        }
+        // if want to use animation that repeats on scroll use this 
+        else {
+            sec.classList.remove('show-animate');
         }
     });
+
     //sticky header
     let header = document.querySelector('header');
 
@@ -35,6 +43,9 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
+    // animation footer on scroll 
+    let footer = document.querySelector('footer');
 
-}
-
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.
+        scrollHeight);
+};
